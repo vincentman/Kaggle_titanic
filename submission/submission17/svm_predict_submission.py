772186d1a67360b67997acb21d659c84d61a7a9c
vincentkao <vincentkao@ibms.sinica.edu.tw>
2018-06-22 16:50:19 +0800

    submission17 backup

---
+++ submission/submission17/svm_predict_submission.py
@@ -0,0 +1,31 @@
+from common import process_data
+from common import process_train_test_data
+from common import process_data_from_Yassine
+import pandas as pd
+from sklearn.externals import joblib
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# turn off warning: SettingWithCopyWarning
+pd.set_option('chained_assignment', None)
+
+# x = pd.read_csv('../test.csv').iloc[:, :]
+# x_test = process_data.get_clean_data(x)
+
+# all_data = process_train_test_data.get_clean_data()
+# test_data = process_train_test_data.get_test_data(all_data)
+
+process_data = process_data_from_Yassine.ProcessData()
+process_data.feature_engineering()
+test_data = process_data.get_test_data()
+x_test = test_data.drop(['Survived'], axis=1)
+
+print('x_test.shape: ', x_test.shape)
+print('x_test.columns => \n', x_test.columns.values)
+
+x_test = StandardScaler().fit_transform(x_test.values)
+
+svm_clf = joblib.load('svm_dump.pkl')
+
+pd.DataFrame({"PassengerId": np.arange(892, 1310), "Survived": svm_clf.predict(x_test).astype(int)}).to_csv(
+    'submission.csv', header=True, index=False)
