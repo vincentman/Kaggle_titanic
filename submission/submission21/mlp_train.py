68c718f54a6ea97fe65423cb5ca48e12b65d8b8a
vincentkao <vincentkao@ibms.sinica.edu.tw>
2018-06-27 08:50:43 +0800

    submission21 backup

--- submission/submission21/mlp_train.py
+++ submission/submission21/mlp_train.py
@@ -1,8 +1,3 @@
-from numpy.random import seed
-seed(1)
-from tensorflow import set_random_seed
-set_random_seed(2)
-
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from common import process_data
